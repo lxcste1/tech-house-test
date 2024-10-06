d9a1bb97e605e8867c8215d63bdea021cc20712e
Lucas <lucas.tello@pinapp.tech>
2024-10-06 13:11:02 -0300

    Corrijo ruteos

--- todo-crud/src/app/components/Header/subcomponents/SubMenu.tsx
+++ todo-crud/src/app/components/Header/subcomponents/SubMenu.tsx
@@ -1,27 +1,44 @@
 "use client";
+import { useState, useEffect } from 'react';
+
 import { usePathname } from 'next/navigation';
 import Link from 'next/link';
+
 import { Button } from '@mui/base/Button';
+
 import { menuItems } from '../utils/utils';
 
 import '@/app/styles/header.scss';
 
 export const SubMenu: React.FC = () => {
-    const pathname = usePathname();
-  
-    return (
-      <div className="relative flex md:justify-center gap-10 px-5 overflow-x-auto whitespace-nowrap scrollbar-hide shadow-md w-full">
-        {menuItems.map(({ label, path }) => (
-          <Link key={label} href={path} className={`${pathname === path ? 'active' : ''}`}>
-            <Button
-              className={`flex items-center mx-2 h-[50px] text-base normal-case ${pathname === path ? 'bg-white text-primary-main font-bold' : 'text-neutral-60 font-semibold'}`}
-            >
-              {label}
-            </Button>
-          </Link>
-        ))}
-      </div>
-    );
-  };
-  
-  export default SubMenu;
+  const pathname = usePathname();
+  const [activeItem, setActiveItem] = useState<string>('Mis tareas');
+
+  useEffect(() => {
+    const currentItem = menuItems.find((item) => item.path === pathname);
+    if (currentItem) {
+      setActiveItem(currentItem.label);
+    }
+  }, [pathname]);
+
+  return (
+    <div className="relative flex md:justify-center gap-10 px-5 overflow-x-auto whitespace-nowrap scrollbar-hide shadow-md w-full">
+      {menuItems.map(({ label, path }) => (
+        <Link key={label} href={path} className={`${activeItem === label ? 'active' : ''}`}>
+          <Button
+            onClick={() => setActiveItem(label)}
+            className={`flex items-center mx-2 h-[50px] text-base normal-case ${
+              activeItem === label
+                ? 'bg-white text-primary-main font-bold'
+                : 'text-neutral-60 font-semibold'
+            }`}
+          >
+            {label}
+          </Button>
+        </Link>
+      ))}
+    </div>
+  );
+};
+
+export default SubMenu;
--- todo-crud/src/app/components/MainContent.tsx
+++ todo-crud/src/app/components/MainContent.tsx
@@ -1,13 +1,14 @@
+import MisTareas from "../mis-tareas/page";
+
 type MainContentProps = {
     selectedItem: string;
-  };
+};
   
   const MainContent: React.FC<MainContentProps> = ({ selectedItem }) => {
     return (
       <div className="p-4">
-        <h2 className="text-2xl font-semibold mb-4">{selectedItem}</h2>
-        {/* Aquí puedes agregar el contenido dinámico que deseas mostrar según la opción seleccionada */}
-        <p>{`Contenido dinámico para ${selectedItem}`}</p>
+        <h2 className="text-2xl font-semibold">{selectedItem}</h2>
+        <MisTareas />
       </div>
     );
   };
